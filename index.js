7728ca069584b9b62eba497604585e1c2b934784
Kirstie <kirstiewithrow@gmail.com>
2021-09-28 23:13:14 -0500

    Completed 9.28

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code here
+let saturdayFun = function saturdayFun(thing = "roller-skate") {
+    return `This Saturday, I want to ${thing}!`;
+};
+function mondayWork(task = "go to the office") {
+    return `This Monday, I will ${task}.`;
+}
+
+function wrapAdjective(wrap = "*") {
+   return function(adjective="a hard worker") {
+    return `You are ${wrap}${adjective}${wrap}!`;
+   }
+}
